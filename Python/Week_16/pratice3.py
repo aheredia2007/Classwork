795a8075be008e6c52e1580a894501ad86240500
adrian <jheredia9@bostonk12.org>
2024-04-25 10:40:07 -0400

    python and html work

---
+++ Python/Week_16/pratice3.py
@@ -0,0 +1,16 @@
+#adrianheredia
+#4/25/24
+
+#this is how you create a empty list
+a=[]
+
+#this is how u create a list with value
+b=[1,2,3]
+
+print(a)
+print(b)
+
+a.append('Glazington')
+b.append(4)
+print(a)
+print(b)
