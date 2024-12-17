cd065fbce42e393edf9bf2d80a39fc6e8c21f336
349284 <jheredia9@bostonk12.org>
2024-12-17 14:10:39 -0500

    programming.py

--- 2024-2025/classwork/python/game.py
+++ 2024-2025/classwork/python/game.py
@@ -47,12 +47,6 @@ def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_hea
     WIN.blit(SPACE, (0, 0))
     pygame.draw.rect(WIN, BLACK, BORDER)
 
-    red_health_text = HEALTH_FONT.render(
-        "Health: " + str(red_health), 1, WHITE)
-    yellow_health_text = HEALTH_FONT.render(
-        "Health: " + str(yellow_health), 1, WHITE)
-    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
-    WIN.blit(yellow_health_text, (10, 10))
 
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x, red.y))
@@ -106,12 +100,7 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
             red_bullets.remove(bullet)
 
 
-def draw_winner(text):
-    draw_text = WINNER_FONT.render(text, 1, WHITE)
-    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
-                         2, HEIGHT/2 - draw_text.get_height()/2))
-    pygame.display.update()
-    pygame.time.delay(5000)
+
 
 
 def main():
@@ -154,16 +143,7 @@ def main():
                 yellow_health -= 1
 
 
-        winner_text = ""
-        if red_health <= 0:
-            winner_text = "Yellow Wins!"
 
-        if yellow_health <= 0:
-            winner_text = "Red Wins!"
-
-        if winner_text != "":
-            draw_winner(winner_text)
-            break
 
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed, yellow)
